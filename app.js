d79ba626ca01fb50637a51b0a653342abedaa3d3
MarioCaly <alejandro239999986@gmail.com>
2023-08-31 07:42:23 -0300

    cambio app.js

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const rutasMain=require("./routes/main.js")
 const app=express();
 const path=require("path")
 app.use(express.static("public"))
-app.listen("3000", function () {
-    console.log("Servidor corriendo")} )
+const port=process.env.PORT || 3001;
+/*app.listen("3000", function () {
+    console.log("Servidor corriendo")} )*/
+app.listen(port,()=>console.log('Sevidor correindo en el puerto ${port}'));
 
 app.use("/",rutasMain); 
 app.use("/productos",rutasProductos)
